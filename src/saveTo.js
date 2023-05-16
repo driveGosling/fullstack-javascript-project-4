b82e92a272df77ad6fce7e7e5f1f88d9a6729e9e
toDoListEnjoyer <toDoListEnjoyer@gmail.com>
2023-05-16 10:33:25 +0300

    add catch block to saveTo

--- src/saveTo.js
+++ src/saveTo.js
@@ -1,5 +1,8 @@
 import fsp from 'fs/promises';
 
-const saveTo = (path, data) => fsp.writeFile(path, data);
+const saveTo = (path, data) => fsp.writeFile(path, data)
+  .catch((err) => {
+    throw new Error(err);
+  });
 
 export default saveTo;
